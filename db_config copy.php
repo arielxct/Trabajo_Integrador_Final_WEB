1c0a21a8ff41f4c8ab25bb09579aaf04ca94d90a
arielxct <campostula.gmail.com>
2023-12-05 15:19:01 -0300

    Actualize archivos

---
+++ db_config copy.php
@@ -0,0 +1,36 @@
+<?php
+// Configuración de la conexión a la base de datos
+$servername = "localhost";
+$username = "id21315121_arielct";
+$password = "codeFullPhp#23";
+$dbname = "id21315121_integrador_php";
+
+// Crear la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$mail = $_POST['mail'];
+$tema = $_POST['tema'];
+$fecha_alta = $_POST['fecha_alta'];
+
+// Preparar la consulta SQL
+$sql = "INSERT INTO oradores (nombre, apellido, mail, tema, fecha_alta) 
+        VALUES ('$nombre', '$apellido', '$mail', '$tema', '$fecha_alta')";
+
+// Ejecutar la consulta
+if ($conn->query($sql) === TRUE) {
+    echo "Registro ingresado con éxito";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar la conexión
+$conn->close();
+?>

1a63ed41757337ec6dc10f1ee89f96c1846ab1e3
MartoJuanDev <martojuandev@gmail.com>
2023-12-07 15:18:58 -0300

    modifico el enfoque para crud, utilizando mysqli

--- procesador_oradores.php
+++ procesador_oradores.php
@@ -11,10 +11,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['accion']) && $_POST['
         // Preparar la consulta SQL para eliminar el registro
         $sqlEliminar = "DELETE FROM oradores WHERE id = $idEliminar";
 
-        if ($conn->query($sqlEliminar) === TRUE) {
+        // Utilizar mysqli_query para ejecutar la consulta
+        if (mysqli_query($conn, $sqlEliminar)) {
             exit();
         } else {
-            echo "Error al eliminar el registro: " . $conn->error;
+            echo "Error al eliminar el registro: " . mysqli_error($conn);
         }
     } else {
         echo "ID no válido para eliminar.";
@@ -31,10 +32,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['accion']) && $_POST['
         // Preparar la consulta SQL para actualizar el registro
         $sqlEditar = "UPDATE oradores SET nombre='$nombre', apellido='$apellido', mail='$mail', tema='$tema' WHERE id=$idEditar";
 
-        if ($conn->query($sqlEditar) === TRUE) {
+        // mysqli_query para ejecutar la consulta
+        if (mysqli_query($conn, $sqlEditar)) {
             exit();
         } else {
-            echo "Error al editar el registro: " . $conn->error;
+            echo "Error al editar el registro: " . mysqli_error($conn);
         }
     } else {
         echo "ID no válido para editar.";
@@ -51,13 +53,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['accion']) && $_POST['
     $sqlInsertar = "INSERT INTO oradores (nombre, apellido, mail, tema, fecha_alta) 
                     VALUES ('$nombre', '$apellido', '$mail', '$tema', NOW())";
 
-    // Ejecutar la consulta de inserción
-    if ($conn->query($sqlInsertar) === TRUE) {
-        // Redirigir a la página oradores.php después de la inserción
+    // mysqli_query para ejecutar la consulta
+    if (mysqli_query($conn, $sqlInsertar)) {
+        // Redirige a la página oradores.php después de la inserción
         header("Location: oradores.php");
         exit();
     } else {
-        echo "Error al insertar el registro: " . $conn->error;
+        echo "Error al insertar el registro: " . mysqli_error($conn);
     }
 }
 
